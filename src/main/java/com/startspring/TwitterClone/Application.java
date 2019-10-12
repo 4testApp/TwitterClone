0ef52719d6c3810c97fb056204bef34e3f6b5c59
VidarrSon <vitharrson@gmail.com>
2019-10-12 17:25:14 +0300

    Start  learning Spring

---
+++ src/main/java/com/startspring/TwitterClone/Application.java
@@ -0,0 +1,20 @@
+package com.startspring.TwitterClone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Map;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+    @GetMapping
+    public String main(Map<String, Object> model) {
+        model.put("some", "Hello, VidarrSon!");
+        return "main";
+    }
+}
