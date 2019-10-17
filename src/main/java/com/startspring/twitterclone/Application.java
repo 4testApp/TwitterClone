ec5a8b135ff920eddd00c395738c6dfd075c124b
VidarrSon <vitharrson@gmail.com>
2019-10-17 12:10:30 +0300

    Add Message Entity and some fixes

--- src/main/java/com/startspring/twitterclone/Application.java
+++ src/main/java/com/startspring/twitterclone/Application.java
@@ -1,4 +1,4 @@
-package com.startspring.TwitterClone;
+package com.startspring.twitterclone;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
