93d90ce79bd9ed1c81a40026b60b974481dd9407
VidarrSon <vitharrson@gmail.com>
2019-11-26 17:53:21 +0200

    Some fixes

--- src/main/java/com/startspring/twitterclone/Application.java
+++ src/main/java/com/startspring/twitterclone/Application.java
@@ -2,9 +2,6 @@ package com.startspring.twitterclone;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-
-import java.util.Map;
 
 @SpringBootApplication
 public class Application {
@@ -12,9 +9,5 @@ public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
-    @GetMapping
-    public String main(Map<String, Object> model) {
-        model.put("some", "Hello, VidarrSon!");
-        return "main";
-    }
+
 }
--- src/main/java/com/startspring/twitterclone/repositories/MessageRepository.java
+++ src/main/java/com/startspring/twitterclone/repositories/MessageRepository.java
@@ -3,6 +3,6 @@ package com.startspring.twitterclone.repositories;
 import com.startspring.twitterclone.domain.Message;
 import org.springframework.data.repository.CrudRepository;
 
-public interface MessageRepository extends CrudRepository<Message, Integer> {
+public interface MessageRepository extends CrudRepository<Message, Long> {
 
 }
